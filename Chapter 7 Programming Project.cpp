02dc1b3b37533bbd3e3ac7f134289b5e4e1a77bd
RScott99 <rsh06.rh@gmail.com>
2025-07-31 11:51:09 -0400

    added txt files to source

--- Chapter 7 Programming Project.cpp
+++ Chapter 7 Programming Project.cpp
@@ -1,6 +1,8 @@
-//
-
+//This program will grade final exams that have 20 multiple choice questions each with 4 possible answers
+//will grade and output their answer, and the expected answer, along with a final score and if they passed or not.
+#include <fstream>
 #include <iostream>
+using namespace std;
 
 int main()
 {
